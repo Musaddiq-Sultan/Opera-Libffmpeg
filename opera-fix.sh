f5efe3a031a40b104456617e38527b87a9abab9e
Musaddiq Sultan <111195207+Musaddiq-Sultan@users.noreply.github.com>
2022-09-07 22:52:27 +0500

    Update opera-fix.sh

--- opera-fix.sh
+++ opera-fix.sh
@@ -5,10 +5,12 @@ if [ $key = 1 ]
 then
 sudo mv /lib/x86_64-linux-gnu/opera/libffmpeg.so /lib/x86_64-linux-gnu/opera/libffmpeg.so.original
 sudo cp libffmpeg.so /lib/x86_64-linux-gnu/opera/
+echo "Changes have been made!"
 elif [ $key = 2 ]
 then
 sudo rm /lib/x86_64-linux-gnu/opera/libffmpeg.so
 sudo mv /lib/x86_64-linux-gnu/opera/libffmpeg.so.original /lib/x86_64-linux-gnu/opera/libffmpeg.so
+echo "Changes have been reversed!"
 elif [ $key != 1 ] || [ $key != 2 ]
 then
 echo "Wrong Input"
